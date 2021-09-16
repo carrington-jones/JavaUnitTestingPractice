1036c9bbb7d1828575af5a786c9bc8b03443219b
Carrington Jones <carrington.jones47@gmail.com>
2021-09-15 23:12:01 -0500

    Added more assertEquals and assertNotEquals tests

--- src/main/java/Company.java
+++ src/main/java/Company.java
@@ -19,4 +19,17 @@ public class Company {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void testIfChangeIsCorrect(){
+        Double price = 10.00;
+        Double discount = 4.50;
+
+        assertEquals(5.5,price - discount, 0);
+        assertEquals(5.1, price - discount,0.5); // This test passes because of margin of error in Delta.
+        assertNotEquals(4.9, price - discount, 0.5); //Test passes because 4.9 is not an expected answer, and we used assertNotEquals
+
+        //When using floating points values a third parameter (Delta) is required. Delta represents positive margin of error of decimals.
+    }
+
 }
