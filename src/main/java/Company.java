32e6bf65557442cc4e2f9213d77fa1f168e05f8d
Carrington Jones <carrington.jones47@gmail.com>
2021-09-16 17:52:18 -0500

    Continued Junit testing studying

--- src/main/java/Company.java
+++ src/main/java/Company.java
@@ -29,7 +29,26 @@ public class Company {
         assertEquals(5.1, price - discount,0.5); // This test passes because of margin of error in Delta.
         assertNotEquals(4.9, price - discount, 0.5); //Test passes because 4.9 is not an expected answer, and we used assertNotEquals
 
-        //When using floating points values a third parameter (Delta) is required. Delta represents positive margin of error of decimals.
+        // When using floating points values a third parameter (Delta) is required. Delta represents positive margin of error of decimals.
     }
+
+    @Test
+    public void testIfObjectsAreDifferent(){
+        Object dog = new Object();
+        Object sheep = new Object();
+        Object clonnedSheep = sheep;
+
+        assertNotSame(sheep, dog); // Passes the test because the objects are NOT the same.
+        assertSame(sheep, clonnedSheep); //Test passes because the objects are the same.
+    }
+
+    @Test
+    public void TestIfArrayEquals(){
+        char[] expected = {'J', 'u', 'n', 'i', 't'};
+        char[] actual = "Junit".toCharArray();
+
+        assertArrayEquals(expected, actual); // Test passes because char[] actual converts "Junit" String to array which equals char[] expected
+    }
+
 
 }
