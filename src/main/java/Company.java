3c7dc0a25024582ca7701e4ff0d7ada5b8abf238
Carrington Jones <carrington.jones47@gmail.com>
2021-09-16 18:20:52 -0500

    Continued Junit testing studying and @Before annotation

--- src/main/java/Company.java
+++ src/main/java/Company.java
@@ -1,4 +1,9 @@
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class Company {
@@ -50,5 +55,47 @@ public class Company {
         assertArrayEquals(expected, actual); // Test passes because char[] actual converts "Junit" String to array which equals char[] expected
     }
 
+    @Test
+    public void testIfGreaterOrLesserThanWorks(){
+        boolean TDDTest = true;
+
+        assertTrue(TDDTest);
+        assertTrue("5 is greater than 4", 5 > 4);
+        assertFalse("5 is not greater than 6", 5 > 6);
+    }
+
+//    @Test //These test fails because the class Device has not been instantiated. The assertNull and NotNull are useful for seeing if an object has been instantiated.
+//    public void testIfInstanceIsNull(){
+//        Device phone = new Device();
+//        Device laptop = null;
+//
+//        assertNull(null);
+//        assertNotNull(phone);
+//    }
+
+
+
+    private List<String> names;
+
+    // When writing test it is common to find that several test need objects or variables created before they are ran. That is where @Before annotation comes in. The @Before methods of superclass will be run before those of teh current class. Similar to a default constructor for plain Java Object class.
+    @Before
+    public void setUp(){
+        this.names = new ArrayList<>();
+        this.names.add("Fer");
+    }
+
+    @Test
+    public void testIfNameIsInitialized(){
+        assertNotNull(names);
+    }
+
+    @Test
+    public void testIfANameCanBeAdded(){
+        assertEquals(1, this.names.size());
+        this.names.add("Zach");
+        assertEquals(2, this.names.size());
+        assertSame("Fer", this.names.get(0));
+        assertSame("Zach", this.names.get(1));
+    }
 
 }
